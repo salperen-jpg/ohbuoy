f524d63e7c25128fcacbde04818fee5c1424590c
salperen <sal.alperen1@gmail.com>
2025-12-06 15:36:29 +0100

    Add navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
 const Navbar = () => {
   return (
     <nav className='border-b '>
-      <div className='px-8 py-4  max-w-7xl mx-auto'>
-        <span>
-          Oh <span className='text-primary capitalize'>buoys</span>
+      <div className='px-8 py-6 max-w-7xl mx-auto'>
+        <span className='font-extrabold text-xl'>
+          Oh <span className='text-orange-600 capitalize'>buoys</span>
         </span>
       </div>
     </nav>
